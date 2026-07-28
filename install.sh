0d5b55c89f51d39d73145fc58567088cd21e98ea
Bamidev <bamidev@pm.me>
2026-07-28 07:40:12 +0200

    Fix the cnpg install

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
+# This file installs everything I need on my Kubernetes cluster.
+set -ex
+
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.36/deploy/local-path-storage.yaml
 
-kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.30/releases/cnpg-1.30.0.yaml
+kubectl apply --server-side -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.30/releases/cnpg-1.30.0.yaml
 
 kubectl apply -f ingress-nginx/deployments/common/ns-and-sa.yaml
 kubectl apply -f ingress-nginx/deployments/rbac/rbac.yaml
