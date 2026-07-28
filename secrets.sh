ddbb0fa7735bebec46db83aec3b47003ceff362d
Bamidev <bamidev@pm.me>
2026-07-28 07:48:11 +0200

    Fix owncast secret

--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 kubectl create secret generic nextcloud --from-literal=secret=$(pass kubernetes/nextcloud/secret) --from-literal=password-salt=$(pass kubernetes/nextcloud/password-salt)
-kubectl create secret generic owncast --from-literal=secret=$(pass kubernetes/owncast/admin)
+kubectl create secret generic owncast --from-literal=admin-password=$(pass kubernetes/owncast/admin)
