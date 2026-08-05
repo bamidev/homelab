2415ead7fefa36e7fb004cbfda21325b4efe7c34
Bamidev <bamidev@pm.me>
2026-08-05 06:54:57 +0200

    Install traefik & democratic-csi

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 # This file installs everything I need on my Kubernetes cluster.
 set -ex
 
+# Helm repos
+helm repo add democratic-csi https://democratic-csi.github.io/charts/
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
 # The Kubernetes Descheduler
 kubectl apply -f descheduler/kubernetes/base/rbac.yaml
 kubectl apply -f descheduler/kubernetes/base/configmap.yaml
 kubectl apply -f descheduler/kubernetes/deployment/deployment.yaml
 
+# Traefik
+helm upgrade --install traefik traefik/traefik --version 41.1.1 -f traefik/values.yaml --wait
+
 # local-path-provisioner so that the CNPG databases can store everything on the disk of the node they are running on.
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.36/deploy/local-path-storage.yaml
 
+# democratic-csi allows Kubernetes to make snapshots
+helm upgrade --install local-zfs democratic-csi/democratic-csi --version 0.15.1 --create-namespace --namespace democratic-csi -f democratic-csi.yaml
+
 # CNPG (CloudNativePostGres)
 kubectl apply --server-side -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.30/releases/cnpg-1.30.0.yaml
-
-# The Nginx ingress controller
-#kubectl apply -f ingress-nginx/deployments/common/ns-and-sa.yaml
-#kubectl apply -f ingress-nginx/deployments/rbac/rbac.yaml
-#kubectl apply -f ingress-nginx/deployments/rbac/crds.yaml
-#kubectl apply -f ingress-nginx/deployments/deployment/nginx-ingress.yaml
-#kubectl apply -f ingress-nginx/deployments/service/loadbalancer.yaml
-
-# Longhorn
-#kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.12.0/deploy/longhorn.yaml
--- secrets.sh
+++ secrets.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+
 kubectl create secret generic nextcloud --from-literal=secret=$(pass kubernetes/nextcloud/secret) --from-literal=password-salt=$(pass kubernetes/nextcloud/password-salt)
 kubectl create secret generic owncast --from-literal=admin-password=$(pass kubernetes/owncast/admin)
