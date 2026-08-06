2cdbdca7286124509336629706012302a8065749
Bamidev <bamidev@pm.me>
2026-08-06 21:59:40 +0200

    Move all apps under the apps folder, and put each app resource into a namespace corresponding with the actual app name

--- secrets.sh
+++ secrets.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl create secret generic nextcloud --from-literal=secret=$(pass kubernetes/nextcloud/secret) --from-literal=password-salt=$(pass kubernetes/nextcloud/password-salt)
-kubectl create secret generic owncast --from-literal=admin-password=$(pass kubernetes/owncast/admin)
+kubectl create secret -n nextcloud generic credentials --from-literal=secret=$(pass kubernetes/nextcloud/secret) --from-literal=password-salt=$(pass kubernetes/nextcloud/password-salt)
+kubectl create secret -n owncast generic credentials --from-literal=admin-password=$(pass kubernetes/owncast/admin)
