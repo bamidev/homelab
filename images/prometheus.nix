0c29b301e78611f57c88cc3ab18252e652584e58
Bamidev <bamidev@pm.me>
2026-08-10 23:01:12 +0200

    Fix the node exporter endpoints for prometheus

--- images/prometheus.nix
+++ images/prometheus.nix
@@ -19,19 +19,31 @@ let
 
       # Nextcloud application metrics
       - job_name: "nextcloud"
-        scrape_interval: 60s         # Nextcloud API can be slow; use longer interval
+        scrape_interval: 60s
         scrape_timeout: 30s
         static_configs:
           - targets: ["service.nextcloud.svc.cluster.local:6399"]
             labels:
               instance: "nextcloud-production"
 
-      # Node Exporter for OS-level metrics
-      - job_name: "node"
+      # All node metrics
+      - job_name: "node-old-laptop-msi"
         static_configs:
-          - targets: ["127.0.0.1:9100"]
+          - targets: ["192.168.0.123:9100"]
             labels:
-              instance: "local-node"
+              instance: "node-old-laptop-msi"
+      - job_name: "node-old-laptop-asus"
+        static_configs:
+          - targets: ["192.168.0.134:9100"]
+            labels:
+              instance: "node-old-laptop-asus"
+      - job_name: "node-thinkcentre"
+        static_configs:
+          - targets: ["192.168.0.148:9100"]
+            labels:
+              instance: "node-thinkcentre"
+      # TODO: Configure the domain names in the nixos-config repo, and us them here somehow.
+      #       Maybe it can be done by importing the flake, and then utilizing the options in config.nix
   '';
 
   entrypointScript = pkgs.writers.writeBashBin "entrypoint.sh" ''
